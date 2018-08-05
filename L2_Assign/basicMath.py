58c164c48873057b383b03c9e1441359f8fd1d41
Anantha RamaKrishnan <arkwork01@gmail.com>
2018-08-05 14:02:31 +0530

    Basic mathematical operations

--- L2_Assign/basicMath.py
+++ L2_Assign/basicMath.py
@@ -1,6 +1,7 @@
 # this file does basic 3D addition, multiplication, div, and subtraction
 # this code uses numpy.random fill array values automatically
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 def do_something(arr_1, arr_2):
@@ -21,14 +22,17 @@ def do_something(arr_1, arr_2):
 
 try:
     while True:
-        fir_arr = np.random.randint(50, size=(3, 3, 3))
-        sec_arr = np.random.randint(50, size=(3, 3, 3))
+        fir_arr = np.random.randint(255, size=(3, 3, 3))
+        sec_arr = np.random.randint(255, size=(3, 3, 3))
         print("FIRST RANDOM ARRAY")
+
         print(fir_arr)
         print("SECOND RANDOM ARRAY")
         print(sec_arr)
         result = do_something(fir_arr, sec_arr)
         print("RESULT")
         print(result)
+        plt.imshow(result, interpolation="nearest")
+        plt.show()
 except KeyboardInterrupt:
     print("USER EXITED")
