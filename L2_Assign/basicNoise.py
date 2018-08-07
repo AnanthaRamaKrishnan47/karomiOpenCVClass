1034a61eb15221cc2b376314dbe81f3af3569750
Anantha RamaKrishnan <arkwork01@gmail.com>
2018-08-07 23:01:57 +0530

    Basic mathematical operations

--- L2_Assign/basicNoise.py
+++ L2_Assign/basicNoise.py
@@ -4,12 +4,13 @@ import numpy as np
 
 
 image = cv.imread(r"C:\Users\arkma\PycharmProjects\karomiOpenCVClass\Resources\Images\mandarin_monkey.PNG")
-row, col, ch = image.shape
-mean = 0
-var = 100
-sigma = var*.5
-gauss = np.random.normal(mean, sigma, (row, col, ch))
-gauss = gauss.reshape(row, col, ch)
+# row, col, ch = image.shape
+# mean = 0
+# var = 100
+# sigma = var*.5
+# gauss = np.random.normal(mean, sigma, (row, col, ch))
+# gauss = gauss.reshape(row, col, ch)
+gauss = np.random.randint(0, 25, image.shape, dtype="uint8")
 noisy = image + gauss
 cv.imshow("Noiseless image", image)
 cv.imshow("Noise introduced image", noisy)
